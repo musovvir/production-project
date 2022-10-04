6a4a1f046fe6b514288359a6cdfc05591c4faf4d
musovvir <mr.muzagov@mail.ru>
2022-10-04 18:10:46 +0300

    add modal, portal, change header

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -26,6 +26,12 @@ Clear.args = {
     theme: ButtonType.CLEAR,
 };
 
+export const ClearInverted = Template.bind({});
+ClearInverted.args = {
+    children: 'Text',
+    theme: ButtonType.CLEAR_INVERTED,
+};
+
 export const Outline = Template.bind({});
 Outline.args = {
     children: 'Text',
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { Modal } from 'shared/ui/Modal/Modal';
+import { Button, ButtonType } from 'shared/ui/Button/Button';
+import { useCallback, useState } from 'react';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -8,11 +11,28 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <div className={cls.links}>
-                /
-            </div>
+            <Button
+                theme={ButtonType.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            {/* eslint-disable-next-line */}
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                lorem ipsum dalor set amet lorem ipsum dalor set amet lorem ipsum dalor set amet
+                lorem ipsum dalor set amet lorem ipsum dalor set amet lorem ipsum dalor set amet
+                lorem ipsum dalor set amet lorem ipsum dalor set amet lorem ipsum dalor set amet
+                lorem ipsum dalor set amet lorem ipsum dalor set amet lorem ipsum dalor set amet
+            </Modal>
         </div>
     );
 };
