1513a9044d81aecff46a6e5e1443053bd4f21b6c
musovvir <mr.muzagov@mail.ru>
2022-10-23 17:33:34 +0300

    fix typings, add profile, fix tests

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -2,3 +2,5 @@ import { profileActions, profileReducer } from './model/slice/profileSlice';
 
 export { Profile, ProfileSchema } from './model/types/profile';
 export { profileActions, profileReducer };
+export { fetchProfileData } from './model/services/fetchProfileData/fetchProfileData';
+export { ProfileCard } from './ui/ProfileCard/ProfileCard';
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -4,7 +4,9 @@ import {
     DynamicModuleLoader,
     ReducersList,
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { useEffect } from 'react';
 
 const reducers: ReducersList = {
     profile: profileReducer,
@@ -17,10 +19,16 @@ interface ProfilePageProps {
 const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation();
 
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
+
     return (
         <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
             <div className={classNames('', {}, [className])}>
-                {t('Profile Page')}
+                <ProfileCard />
             </div>
         </DynamicModuleLoader>
     );
