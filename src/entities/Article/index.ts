f08bc250abce324dfb2f20e9e6ac64fbc3212d08
musovvir <mr.muzagov@mail.ru>
2022-11-15 00:22:48 +0300

    add fetching articles, view types selector

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -3,3 +3,4 @@ export { Article, ArticleView } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export { getArticleDetailsData } from './model/selectors/articleDetails';
 export { ArticleList } from './ui/ArticleList/ArticleList';
+export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
